a4cf3c691ce764855893548ab7151664fafc1d9f
switzmab <switzmab@mcmaster.ca>
2024-11-26 22:47:53 -0500

    Fixed small things in I2A

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -19,84 +19,81 @@ int objPosArrayList::getSize() const
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-    if(listSize == arrayCapacity)
+    if(listSize != arrayCapacity)
     {
-        return;
+        for (int i = listSize; i > 0; i--)
+        {
+            aList[i] = aList[i - 1];
+        }
+        aList[0] = thisPos;
+        listSize++;
     }
 
-    for(int i = listSize; i > 0; i--)
-    {
-        aList[i] = aList[i-1];
-    }
-    aList[0] = thisPos;
-    listSize++;
+    
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-    if(listSize == arrayCapacity)
+    if(listSize != arrayCapacity)
     {
-        return;
+        aList[listSize++] = thisPos;
     }
 
-    aList[listSize++] = thisPos; 
+    
 }
 
 void objPosArrayList::removeHead()
 {
-    if(listSize == 0)
+    if(listSize != 0)
     {
-        return;
+        for (int i = 0; i < listSize - 1; i++)
+        {
+            aList[i] = aList[i + 1];
+        }
+
+        listSize--;
     }
 
-    for(int i = 0; i < listSize - 1; i++)
-    {
-        aList[i] = aList[i+1];
-    }
-
-    listSize--;
+    
 }
 
 
 void objPosArrayList::removeTail()
 {
-    if(listSize == 0)
+    if(listSize != 0)
     {
-        return;
+        listSize--;
     }
 
-    listSize--;
+    
 }
 
 
 objPos objPosArrayList::getHeadElement() const
 {
-    if(listSize == 0)
+    if(listSize != 0)
     {
-        return;
+        return aList[0];
     }
-
-    return aList[0];
+    
 }
 
 objPos objPosArrayList::getTailElement() const
 {
-    if(listSize == 0)
+    if(listSize != 0)
     {
-        return;
+        return aList[listSize - 1];
     }
-
-    return aList[listSize - 1];
 }
 
 objPos objPosArrayList::getElement(int index) const
 {
-    if(listSize == 0 || index < 0 || index >= listSize)
+    if(listSize != 0 && index >= 0 && index < listSize)
     {
-        return;
+        return aList[index];
     }
 
-    return aList[index];
+    
 }
 // Paste your Tested implementation here.
 // Paste your Tested implementation here.
